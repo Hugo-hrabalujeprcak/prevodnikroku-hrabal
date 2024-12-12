dff090174db1340344aee5717a38c5f4a047506b
h.hrabal <h.hrabal@PC-IVT11>
2024-12-12 17:27:40 +0900

    Přidejte soubory projektu.

---
+++ prevodnikroku hrabal/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int pocetRoku = 2;
+int pocetMesicu = pocetRoku * 12;
+double pocetDni = pocetRoku * 365.25;
+double pocetHodin = pocetDni * 24;
+double pocetMinut = *60;
+double pocetSekund = pocetMinut * 60;
+
+Console.WriteLine("------------------");
+Console.WriteLine("Roky: " + pocetRoku);
+Console.WriteLine("Měsíce:" + pocetMesicu);
+
+
+Console.ReadKey(); //toto bude poslední tvůj 
